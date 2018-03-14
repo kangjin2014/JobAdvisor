62e1ee0b3ecda139a147723dcc903ef4a1d600f6
jin kang <jinkang@jins-MacBook-Pro.local>
2018-03-13 22:16:03 -0400

    modify run_elasticsh.sh

--- bin/run_elasticsearch.sh
+++ bin/run_elasticsearch.sh
@@ -1,2 +1,3 @@
-#!/bash/bin
-docker run --name elastic0 -p 9200:9200 docker.elastic.co/elasticsearch/elasticsearch:6.2.2
+#!/bin/bash
+docker run --name es_0 -p 9200:9200 docker.elastic.co/elasticsearch/elasticsearch:6.2.2
+echo the elastichsearch is spinned up at port 9200
