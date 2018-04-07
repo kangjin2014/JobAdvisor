78705e19bc39ab9a78507d16e9fa99abf800c363
jin kang <jinkang@jins-MacBook-Pro.local>
2018-04-07 14:06:36 -0400

    add flaskr folder

--- bin/run_flask.sh
+++ bin/run_flask.sh
@@ -2,8 +2,8 @@
 #Compiled by Ryan
 #the flask will pip install, and run the flask task flask/examples/flaskr/flaskr/factory_submit.py
 
-cd flask/examples/flaskr/
-pip install --editable .
+pip install --editable flask/examples/flaskr/.
 export FLASK_APP="flaskr.factory_submit"
+echo $(pwd)
 flask run --port=5010 # avoid port 5000
 echo Find the website on 'localhost:5010'
--- resume_matching/__init__.py
+++ resume_matching/__init__.py
@@ -4,13 +4,13 @@ import pandas as pd
 
 def init(mode):
     '''
-    'elasticsearch mode' : use elasticsearch to do the text matching
+    'elasticsearch mode' : use elasticsearch to do the text matching - default
     'batch mode': compare batch job posting file to resume using cosine similarity calculation
     '''
     
     lf = rm.LoadFiles()
     df_skills = lf.load_skills_dict('data/key_skill.csv')  
-    resume  = lf.load_resume('data/resume_ryan_kang.docx')
+    resume  = lf.load_resume('/tmp/uploaded_resume.docx')
     df_skills = rm.prep_skills_dict(df_skills)
 
     if mode == 'elasticsearch':
@@ -25,3 +25,5 @@ def init(mode):
                                                                 axis = 1).sort_values(ascending = False)                                                             
         df_recommend = df_jobs.loc[list_cosine_similarity.index][0:10]
         print ('The jobs recommended to you are {}'.format(df_recommend))
+
+    return df_recommend.head(3)
