99ea856dc059c46214610950b5c5cb7bf63e4af1
jin kang <jinkang@jins-MacBook-Pro.local>
2018-04-07 13:17:10 -0400

    github

--- flask/examples/flaskr/flaskr/factory_submit.py
+++ flask/examples/flaskr/flaskr/factory_submit.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
+from werkzeug.datastructures import FileStorage
+import os
+
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = '/tmp'
 
 @app.route('/')
 def upload_file():
-   return render_template('upload.html')
+    return render_template('upload.html')
 	
 @app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file_0():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+def show_uploaded():
+      if request.method == 'POST':
+            file = request.files['file']
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            return  render_template('result.html', file = type(filename))
 		
 if __name__ == '__main__':
    app.run(debug = True)
