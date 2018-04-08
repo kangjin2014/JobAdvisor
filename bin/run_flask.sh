320dc9c906b22727b9445b2495c375226857c7ec
Ryan_Kang <kangjin2014@gmail.com>
2018-04-08 14:52:06 -0400

    Delete run_flask.sh

--- bin/run_flask.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Compiled by Ryan
-#the flask will pip install, and run the flask task flask/examples/flaskr/flaskr/factory_submit.py
-
-pip install --editable flask/examples/flaskr/.
-export FLASK_APP="flaskr.factory_submit"
-echo $(pwd)
-flask run --port=5010 # avoid port 5000
-echo Find the website on 'localhost:5010'
